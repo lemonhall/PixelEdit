338639f2b744e4ab27143d802b632c000fb39ff5
Max <photos482@gmail.com>
2013-08-21 15:47:21 -0400

    Added two graphical selectable tools in the left sidebar.

--- pixeledit.js
+++ pixeledit.js
@@ -5,6 +5,63 @@ var img;
 var currentColor;
 var currentTool;
 
+//Tools sprites (made in PixelEdit!!)
+var pencil = {
+    width: 8,
+    height: 20,
+    colors: ["pink", "yellow", "#FFCF91", "black"],
+    pxmap: [
+        [-1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, 0, 0, 0, 0, 0, 0, -1],
+        [-1, 0, 0, 0, 0, 0, 0, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 1, 1, 1, 1, 1, 1, -1],
+        [-1, 2, 2, 2, 2, 2, 2, -1],
+        [-1, -1, 2, 2, 2, 2, -1, -1],
+        [-1, -1, -1, 3, 3, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, -1]
+    ]
+};
+var eraser = {
+    width: 15,
+    height: 10,
+    colors: ["#FF94E4", "black"],
+    pxmap: [
+        [-1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1, 1, 1, 0, 1, 1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, 1, 1, 0, 0, 0, 0, 0, 1, -1, -1],
+        [-1, -1, -1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1],
+        [-1, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, -1],
+        [-1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, -1, -1, -1],
+        [-1, 1, 0, 0, 0, 0, 0, 0, 0, 1, -1, -1, -1, -1, -1],
+        [-1, -1, 1, 0, 0, 0, 0, 1, 1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, 1, 0, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+    ]
+};
+var arrow = {
+    width: 5,
+    height: 3,
+    colors: ["red"],
+    pxmap: [
+        [-1, -1, 0, -1, -1],
+        [-1, 0, 0, 0, -1],
+        [0, 0, 0, 0, 0]
+    ]
+};
+
+//Begin pixeledit
 window.onload = function(){
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext('2d');
@@ -42,9 +99,22 @@ function drawui(){
 	
 	ctx.fillStyle = "#ebebeb";	//Left toolbar
 	ctx.fillRect(0, 0, 80, canvas.height-80);
+		drawSpr(pencil, 25, 20, 3);			//and its tools
+		drawSpr(eraser, 15, 110, 3);
+	
 	
 	ctx.fillStyle = currentColor;	//bottom corner
 	ctx.fillRect(0, canvas.height-80, 80, 80);
+}
+function drawSpr(sprt, x, y, sz){	//draws pixeledit sprites
+	for(var ly=0; ly<sprt.height; ly++){
+		for(var lx=0; lx<sprt.width; lx++){
+			if(sprt.pxmap[ly][lx] != -1){
+				ctx.fillStyle = sprt.colors[sprt.pxmap[ly][lx]];
+				ctx.fillRect(x + (lx*sz), y + (ly*sz), sz, sz);
+			}
+		}
+	}
 }
 function addPx(x, y){
 	//get square mouse is on
@@ -120,7 +190,12 @@ function drawPxMap(px){
 
 function detectClickIntent(x, y){
 	if(x<80 && y<320){	//Left toolbar
-		currentTool = +prompt("tool? 0=pencil, 1=eraser");
+		if(y<90){
+			currentTool = 0;
+		}
+		else if(y>40 && y<150){
+			currentTool = 1;
+		} 
 	}
 	else if(x<80 && y>320){	//color picker
 	
@@ -141,9 +216,11 @@ function Clicked(){
 	    var event = event || window.event,
 	    x = event.pageX - canvas.offsetLeft,
         y = event.pageY - canvas.offsetTop;
+        alert(x + ", " + y);
         detectClickIntent(x, y);
 }
-function exportImg(eimg){	//Export to a js object
+
+function exportImg(eimg){	//Export to a js string object
 	var exported = "";	
 	exported = "var img = { width:" + eimg.width + ", height: " + eimg.height + ", ";	//initial properties
 	var ecolors = "[";	//add colors
